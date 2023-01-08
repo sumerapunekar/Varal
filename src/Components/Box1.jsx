4e47b2829b171ece8f11ace72e7c9449d4403969
SUMERA PUNEKAR <sumerapunekar1999@gmail.com>
2023-01-08 10:45:02 +0530

    done

---
+++ src/Components/Box1.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Box1() {
+  return (
+    <div>
+         <div className='flex flex-col space-y-4'>
+                    <img className='h-10 w-10' src="/Assets/dungeon.png"/>
+                    <h3 className='text-xl font-bold  '>UAE Free Zone Company</h3>
+                    <p className='text-base text-gray-700'>Your registration agent, will answer all<br/>
+                       of your questions and help you to reach<br/>
+                       a conclusion that meets your objectives<br/>
+                       of investing in the UAE.</p>
+                    <h3 className='text-xl text-amber-400'>Get Started</h3>
+                </div>
+    </div>
+  )
+}
+
+export default Box1
